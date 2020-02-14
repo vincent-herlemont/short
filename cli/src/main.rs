1d6e2e7f73548e7b61524bf56aa373625f15880a
Vincent Herlemont <vincentherl@leszeros.com>
2020-02-14 16:34:32 +0000

    add clap

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,16 @@
+use clap::App;
+use clap::AppSettings::ArgRequiredElseHelp;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const BIN_NAME: &'static str = "d4d";
+
 fn main() {
-    println!("Hello, world!");
+    App::new(BIN_NAME)
+        .setting(ArgRequiredElseHelp)
+        .bin_name(BIN_NAME)
+        .version(VERSION)
+        .subcommand(App::new("add").about("add preset configuration"))
+        .subcommand(App::new("watch").about("watch cloudformation infrastructure"))
+        .subcommand(App::new("status").about("display of cloud formation infrastructure"))
+        .get_matches();
 }
