a5458b87881b54dbd5a991c59d2ff1e52caf6ad6
Vincent Herlemont <vincentherl@leszeros.com>
2020-03-01 07:28:16 +0000

    wait ... : stack

--- d4d/src/cloudformation/stack.rs
+++ d4d/src/cloudformation/stack.rs
@@ -2,9 +2,11 @@
 //! Stack is the remote model retrieve from AWS cloud (via CLI or API)
 use std::cell::RefCell;
 
+#[allow(dead_code)]
 pub struct Stack {
     pub nested: Vec<RefCell<Stack>>,
     pub content_stack: ContentStack,
 }
 
+#[allow(dead_code)]
 pub struct ContentStack {}
