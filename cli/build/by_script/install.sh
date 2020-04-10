3f300669630079d6310dfbec8e45574bd1377d11
Vincent Herlemont <vincentherl@leszeros.com>
2020-04-10 07:28:01 +0000

    install by script + readme

--- cli/build/by_script/install.sh
+++ cli/build/by_script/install.sh
@@ -1,2 +1,35 @@
 #!/bin/sh
-echo "install ...";
+install_directory=/usr/bin
+
+if ! [ -x `command -v git` ]; then
+  echo "git command not found (please install git)";
+  exit 1;
+fi
+
+if ! [ -x `command -v aws` ]; then
+  echo "aws command not found (please install aws cli)";
+  exit 1;
+fi
+
+if ! [ -x `command -v cargo` ]; then
+  echo "cargo command not found (please install rust/cargo)";
+  exit 1;
+fi
+
+if ! [ -d short ]; then
+  git clone https://github.com/vincent-herlemont/short.git
+fi
+
+release_file=target/release/short
+
+cd short
+
+if ! [ -f $release_file ]; then
+  cargo build --release --frozen --all-features
+fi
+
+if [ -d $install_directory ]; then
+  sudo chmod 755 $release_file
+  sudo cp $release_file $install_directory
+fi
+
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Short
 
 ![](https://github.com/vincent-herlemont/short/workflows/Rust/badge.svg)
+
+## Install
+
+Linux : Run this command on terminal
+```bash
+curl https://raw.githubusercontent.com/vincent-herlemont/short/master/cli/build/by_script/install.sh | sh
+```
