02dbdfb15a4e20c52416e577c2c7131777938e23
Vincent Herlemont <vincentherl@leszeros.com>
2020-05-31 17:09:05 +0000

    rename command: clean

--- src/cli/commands/rename.rs
+++ src/cli/commands/rename.rs
@@ -8,12 +8,8 @@ pub fn rename(app: &ArgMatches) -> Result<()> {
     let mut cfg = get_cfg()?;
     cfg.sync_local_to_global()?;
 
-    let last_setup_name = app
-        .value_of("last_setup_name")
-        .context("last setup name can not have no UTF-8 string")?;
-    let new_setup_name = app
-        .value_of("new_setup_name")
-        .context("new setup name can not have no UTF-8 string")?;
+    let last_setup_name = app.value_of("last_setup_name").unwrap();
+    let new_setup_name = app.value_of("new_setup_name").unwrap();
 
     let setup = cfg.current_setup(&last_setup_name.to_string())?;
 
