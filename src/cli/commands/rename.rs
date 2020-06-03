732025731a88cfc4a6c7d6c85d9e258a61fa881d
Vincent Herlemont <vincentherl@leszeros.com>
2020-06-03 07:38:44 +0000

    command: rename, use current setup

--- src/cli/commands/rename.rs
+++ src/cli/commands/rename.rs
@@ -1,12 +1,15 @@
+use super::r#use::use_workflow;
 use crate::cli::cfg::get_cfg;
-
+use crate::cli::settings::get_settings;
 use crate::cli::terminal::message::success;
-use anyhow::{Result};
+use anyhow::Result;
 use clap::ArgMatches;
 
 pub fn rename(app: &ArgMatches) -> Result<()> {
     let mut cfg = get_cfg()?;
     cfg.sync_local_to_global()?;
+
+    let mut settings = get_settings(&app, &cfg);
 
     let last_setup_name = app.value_of("last_setup_name").unwrap();
     let new_setup_name = app.value_of("new_setup_name").unwrap();
@@ -14,6 +17,13 @@ pub fn rename(app: &ArgMatches) -> Result<()> {
     let setup = cfg.current_setup(&last_setup_name.to_string())?;
 
     setup.rename(&new_setup_name.to_string())?;
+
+    if let Ok(current_setup_name) = settings.setup() {
+        if last_setup_name == current_setup_name {
+            settings.set_setup(new_setup_name.to_string());
+            use_workflow(&cfg, &settings)?;
+        }
+    }
 
     cfg.save()?;
 
--- tests/rename.rs
+++ tests/rename.rs
@@ -9,10 +9,11 @@ mod utils;
 fn cmd_rename() {
     let itew = IntegrationTestEnvironmentWrapper::init_all("cmd_init");
     {
+        let local_cfg = itew.get_rel_path(PathTestEnvironment::LocalCfg).unwrap();
         let e = itew.e();
         let mut e = e.borrow_mut();
         e.add_file(
-            &itew.get_rel_path(PathTestEnvironment::LocalCfg).unwrap(),
+            &local_cfg,
             r#"
 setups:
   - name: setup_1
@@ -44,6 +45,65 @@ setups:
 
         let r = e.read_file(itew.get_rel_path(PathTestEnvironment::GlobalCfg).unwrap());
         assert!(!contains("setup_1").eval(&r));
+        assert!(contains("setup_2").count(1).eval(&r));
+    }
+}
+
+#[test]
+fn cmd_rename_with_use() {
+    let itew = IntegrationTestEnvironmentWrapper::init_all("cmd_init");
+    {
+        let local_cfg = itew.get_rel_path(PathTestEnvironment::LocalCfg).unwrap();
+        let local_abs_cfg = itew.get_abs_path(PathTestEnvironment::LocalCfg).unwrap();
+        let e = itew.e();
+        let mut e = e.borrow_mut();
+        e.add_file(
+            &local_cfg,
+            r#"
+setups:
+  - name: setup_1
+    file: run.sh
+    array_vars: {}
+        "#,
+        );
+        e.add_file(
+            &itew.get_rel_path(PathTestEnvironment::GlobalCfg).unwrap(),
+            format!(
+                r#"---
+projects:
+  - file: {}
+    current:
+      setup: setup_1
+    setups:
+      - name: setup_1
+            "#,
+                &local_abs_cfg.to_string_lossy()
+            ),
+        );
+        e.setup();
+    }
+
+    let mut command = itew.command(env!("CARGO_PKG_NAME"));
+    let r = command
+        .env("RUST_LOG", "debug")
+        .arg("rename")
+        .arg("setup_1")
+        .arg("setup_2")
+        .assert()
+        .success()
+        .to_string();
+
+    assert!(contains("setup renamed").eval(&r));
+
+    {
+        let e = itew.e();
+        let e = e.borrow_mut();
+        let r = e.read_file(itew.get_rel_path(PathTestEnvironment::LocalCfg).unwrap());
+        assert!(!contains("setup_1").eval(&r));
         assert!(contains("setup_2").eval(&r));
+
+        let r = e.read_file(itew.get_rel_path(PathTestEnvironment::GlobalCfg).unwrap());
+        assert!(!contains("setup_1").eval(&r));
+        assert!(contains("setup_2").count(2).eval(&r));
     }
 }
